2fc077f62caa3ce3a6c1798bfb3936f02b10e8ec
Alexandre Faustino <alexmigf@gmail.com>
2024-09-06 09:21:15 +0100

    Update woocommerce-pushover.php

--- woocommerce-pushover.php
+++ woocommerce-pushover.php
@@ -1,27 +1,18 @@
 <?php
 /**
  * Plugin Name:          Pushover Integration for WooCommerce
+ * Requires Plugins:     woocommerce
  * Plugin URI:           https://shopplugins.com/
  * Description:          Integrates WooCommerce with Pushover.net, a versatile push notification service that delivers real-time alerts to Android, iOS, and desktop devices.
  * Version:              1.0.20
  * Author:               Shop Plugins
  * Author URI:           https://shopplugins.com/
+ * License:              GPL-3.0
+ * License URI:          https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:          wc_pushover
  * WC requires at least: 3.0
  * WC tested up to:      9.2
 */
-/**
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
 
 /**
  * Required functions
